cd925ce21b492360c62ce1b00e49d07a2e6e1d0b
Louis <836250617@qq.com>
2024-05-19 04:41:27 +0800

    async/await

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 #![no_std]
 #![no_main]
+#![allow(unreachable_code)] // FIXME: remove this line in the future
 #![feature(custom_test_frameworks)]
 #![test_runner(blog_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 extern crate alloc;
 
-use blog_os::{allocator::init_heap, memory::BootInfoFrameAllocator, println};
+use blog_os::{
+    allocator::init_heap,
+    memory::BootInfoFrameAllocator,
+    println,
+    task::{executor::Executor, keyboard, Task},
+};
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 use x86_64::VirtAddr;
@@ -23,6 +29,11 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     let mut mapper = unsafe { memory::init(phys_mem_offset) };
     let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
     init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
+
+    let mut executor = Executor::new();
+    executor.spawn(Task::new(keyboard::print_keypresses()));
+    executor.run();
+
     #[cfg(test)]
     test_main();
     println!("It didn't crash");
